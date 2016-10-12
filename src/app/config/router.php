2c00d86e387121fc0040660abcb13b65cb387b1a
Alex Lich <psixwill@gmail.com>
2016-10-13 00:45:06 +0300

    изменили работу функции роутинг с запросами

--- src/app/config/router.php
+++ src/app/config/router.php
@@ -1,75 +1,93 @@
 <?php
 namespace App\Config;
-use AltoRouter;
+use App\Core\Route;
+use App\Core\RouteContext;
 class Router
 {
     protected $router;
 
     function __construct()
     {
-        $this->router = new AltoRouter();
+        $this->router = new Route();
 
-        $this->router->map('get', '/', function() {
-            $this->routingController('CommentsController', 'index');
-        });
+        // $this->router->map('get', '/[i:id]', function() {
+        //     $this->routingController('CommentsController', 'index');
+        // });
 
-        $this->router->map('get', '/descname', function() {
-            $this->routingController('CommentsController', 'descname');
-        });
+        // $this->router->map('get', '/', 'CommentsController', 'index', $query);
+        // });
 
-        $this->router->map('get', '/ascname', function() {
-            $this->routingController('CommentsController', 'ascname');
-        });
-
-        $this->router->map('get', '/descemail', function() {
-            $this->routingController('CommentsController', 'descemail');
-        });
-
-        $this->router->map('get', '/ascemail', function() {
-            $this->routingController('CommentsController', 'ascemail');
-        });
-
-        $this->router->map('get', '/descdate', function() {
-            $this->routingController('CommentsController', 'descdate');
-        });
-
-        $this->router->map('get', '/ascdate', function() {
-            $this->routingController('CommentsController', 'ascdate');
-        });
-
-        $this->router->map('post', '/add', function() {
-            $this->routingController('CommentsController', 'add');
-        });
-
+        // $this->router->map('get', '/descname', function() {
+        //     $this->routingController('CommentsController', 'descname');
+        // });
+        //
+        // $this->router->map('get', '/ascname', function() {
+        //     $this->routingController('CommentsController', 'ascname');
+        // });
+        //
+        // $this->router->map('get', '/descemail', function() {
+        //     $this->routingController('CommentsController', 'descemail');
+        // });
+        //
+        // $this->router->map('get', '/ascemail', function() {
+        //     $this->routingController('CommentsController', 'ascemail');
+        // });
+        //
+        // $this->router->map('get', '/descdate', function() {
+        //     $this->routingController('CommentsController', 'descdate');
+        // });
+        //
+        // $this->router->map('get', '/ascdate', function() {
+        //     $this->routingController('CommentsController', 'ascdate');
+        // });
+        //
+        // $this->router->map('post', '/add', function() {
+        //     $this->routingController('CommentsController', 'add');
+        // });
+        //
+        // $this->router->map('get', '/edit/[i:id]/', function() {
+        //     $this->routingController('CommentsController', 'add');
+        // });
         //Пути роутинга прописать
+
+        $this->router->map('get', '/', array('CommentsController', 'index'));
     }
 
     public function routing()
     {
         $match = $this->router->match();
-        $target = $match['target'];
-        $param = $match['params'];
 
-        if($match && is_callable( $match['target'])) {
-            call_user_func_array( $match['target'], $match['params'] );
-        } else {
-            // no route was matched
-            $this->NotFound();
-        }
+        $target = $match['target'];
+
+        $paramMatch = $match['params'];
+
+        $queryMatch = $match['query'];
+
+        $context = new RouteContext($paramMatch, $queryMatch);
+
+        $this->routingController($context, $target);
+        //var_dump($quering);
+
+        // if($match && is_callable($target)) {
+        //     call_user_func($target, $quering);
+        // } else {
+        //     // no route was matched
+        //     $this->NotFound();
+        // }
     }
 
     //Не доработанная функция на ошибок
-    public function routingController($controllerName, $action, $param = null)
+    public function routingController($context, $target)
     {
+        $controllerName = $target[0];
+        $action = $target[1];
+
         $controllerName = "App\\Controller\\".$controllerName;
+
         if (class_exists($controllerName)) {
-            $controller = new $controllerName;
+            $controller = new $controllerName($context);
             if (method_exists($controller, $action)) {
-                if (is_null($param)) {
-                    $controller->$action();
-                }  else {
-                    $controller->$action($param);
-                }
+                $controller->$action();
             }
         } else {
             $this->NotFound();
