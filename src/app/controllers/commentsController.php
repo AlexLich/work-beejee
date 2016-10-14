c2a18157e5af02e5dedea4e624cb045ab29384ca
Alex Lich <psixwill@gmail.com>
2016-10-14 19:49:30 +0300

    закоментили лишнии функции сортировки,добавили получение авторизации

--- src/app/controllers/commentsController.php
+++ src/app/controllers/commentsController.php
@@ -3,19 +3,19 @@ namespace App\Controller;
 
 use App\Core\Controller;
 use App\Service\CommentsService;
-//use App\Service\AuthService;
+use App\Service\AuthService;
 use App\Model\Comment;
 
 class CommentsController extends Controller
 {
-    /*protected $authService;*/
+    protected $authService;
     protected $commentsService;
 
     function __construct($context)
     {
         parent::__construct($context);
         $this->commentsService = new CommentsService();
-        /*$this->authService = new AuthService();*/
+        $this->authService = new AuthService();
     }
 
     public function add()
@@ -37,6 +37,8 @@ class CommentsController extends Controller
 
     public function index()
     {
+        $isAuth = $this->authService->isAuth();
+
         $query = $this->context->getQueries();
 
         $sort = isset($query['sort']) ? $query['sort'] : "created_at";
@@ -46,49 +48,49 @@ class CommentsController extends Controller
 
         #при вызове getall ошибка, но код будет дальше или хз, или предупредить клиенту что не нашли или упс.
 
-        $data = array('comments' => $comments);
+        $data = array('comments' => $comments, 'isAuth' => $isAuth);
         $this->view->render('comments.html.twig', $data);
     }
 
-    public function descname()
-    {
-        $comments = $this->commentsService->sortdecsname();
-        $data = array('comments' => $comments);
-        $this->view->render('comments.html.twig', $data);
-    }
-
-    public function ascname()
-    {
-        $comments = $this->commentsService->sortacsname();
-        $data = array('comments' => $comments);
-        $this->view->render('comments.html.twig', $data);
-    }
-
-    public function descemail()
-    {
-        $comments = $this->commentsService->sortdecsemail();
-        $data = array('comments' => $comments);
-        $this->view->render('comments.html.twig', $data);
-    }
-
-    public function ascemail()
-    {
-        $comments = $this->commentsService->sortacsemail();
-        $data = array('comments' => $comments);
-        $this->view->render('comments.html.twig', $data);
-    }
-
-    public function descdate()
-    {
-        $comments = $this->commentsService->sortdecsdate();
-        $data = array('comments' => $comments);
-        $this->view->render('comments.html.twig', $data);
-    }
-
-    public function ascdate()
-    {
-        $comments = $this->commentsService->sortacsdate();
-        $data = array('comments' => $comments);
-        $this->view->render('comments.html.twig', $data);
-    }
+    // public function descname()
+    // {
+    //     $comments = $this->commentsService->sortdecsname();
+    //     $data = array('comments' => $comments);
+    //     $this->view->render('comments.html.twig', $data);
+    // }
+    //
+    // public function ascname()
+    // {
+    //     $comments = $this->commentsService->sortacsname();
+    //     $data = array('comments' => $comments);
+    //     $this->view->render('comments.html.twig', $data);
+    // }
+    //
+    // public function descemail()
+    // {
+    //     $comments = $this->commentsService->sortdecsemail();
+    //     $data = array('comments' => $comments);
+    //     $this->view->render('comments.html.twig', $data);
+    // }
+    //
+    // public function ascemail()
+    // {
+    //     $comments = $this->commentsService->sortacsemail();
+    //     $data = array('comments' => $comments);
+    //     $this->view->render('comments.html.twig', $data);
+    // }
+    //
+    // public function descdate()
+    // {
+    //     $comments = $this->commentsService->sortdecsdate();
+    //     $data = array('comments' => $comments);
+    //     $this->view->render('comments.html.twig', $data);
+    // }
+    //
+    // public function ascdate()
+    // {
+    //     $comments = $this->commentsService->sortacsdate();
+    //     $data = array('comments' => $comments);
+    //     $this->view->render('comments.html.twig', $data);
+    // }
 }
