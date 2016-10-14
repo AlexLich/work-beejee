91470bfc0de535ed71bde2af1ae9fe159e45ef64
Alex Lich <psixwill@gmail.com>
2016-10-14 19:51:00 +0300

    создали контроллер авторизации

---
+++ src/app/controllers/authController.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Controller;
+
+use App\Core\Controller;
+use App\Service\AuthService;
+
+class AuthController extends Controller
+{
+    protected $authService;
+
+    public function __construct($context)
+    {
+        parent::__construct($context);
+        $this->authService = new AuthService;
+    }
+
+    public function index()
+    {
+        $this->view->render('login.html.twig');
+    }
+
+    public function login()
+    {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+        $isAuth = $this->authService->login($username, $password);
+
+        header("Location: /");
+    }
+
+    public function logout()
+    {
+        $this->authService->logout();
+        header("Location: /");
+    }
+}
