bd973f24652f0d37df2159798cc9e8a1bf6493b9
Alex Lich <psixwill@gmail.com>
2016-10-14 20:24:18 +0300

    рефакторинг

--- src/app/config/connect.php
+++ src/app/config/connect.php
@@ -19,7 +19,7 @@ class Connect
         try
         {
             $db = new PDO("mysql:host=$db_host;dbname=$db_name;", $db_user, $db_pass);
-//            $db->query("SET NAMES 'utf8'");
+
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
             catch(PDOException $e) {
