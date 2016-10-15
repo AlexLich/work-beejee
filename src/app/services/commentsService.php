589f8b3a500b2748570273b60a5e5230d1132d32
Alex Lich <psixwill@gmail.com>
2016-10-15 04:24:41 +0300

    добавлены фнкцияи получения выбранного отзыва и апдейт

--- src/app/services/commentsService.php
+++ src/app/services/commentsService.php
@@ -3,6 +3,7 @@ namespace App\Service;
 
 use App\Config\Connect;
 use App\Service\AuthService;
+use App\Model\Comment;
 
 
 use PDO;
@@ -23,7 +24,7 @@ class CommentsService
         $data = null;
 
         if ($isAuth) {
-            $sql="SELECT username, body,email, created_at, accepted, changed_by_admin  FROM comments ORDER by $sort $orderby";
+            $sql="SELECT id, username, body,email, created_at, accepted, changed_by_admin  FROM comments ORDER by $sort $orderby";
         }else {
             $sql="SELECT username, body,email, created_at, changed_by_admin  FROM comments WHERE accepted = 1 ORDER by $sort $orderby";
         }
@@ -45,6 +46,44 @@ class CommentsService
         if(!is_null($pdo)) {
             $sth = $pdo->prepare($sql);
             $count = $sth->execute(array(':username' => $comments->username,':email' => $comments->email, ':body' => $comments->body));
+            $pdo = null;
+        }
+        return $count;
+    }
+
+    public function getById($id)
+    {
+        // $isAuth = $this->authService->isAuth();
+
+        $data = null;
+        $sql="SELECT id,username, body,email, created_at, changed_by_admin  FROM comments WHERE id='$id'";
+
+        $pdo = $this->connect->getDb();
+        if(!is_null($pdo)) {
+            $data = $pdo->query($sql)->fetch();
+            $pdo = null;
+        }
+
+        $comments = new Comment();
+
+        $comments->id = $data["id"];
+        $comments->email = $data["email"];
+        $comments->username = $data["username"];
+        $comments->body = $data["body"];
+
+        return $comments;
+    }
+
+    public function update($comment)
+    {
+        $count = 0;
+
+        $sql='UPDATE comments set email=:email, username=:username, body=:body, changed_by_admin=1 where id=:id';
+
+        $pdo = $this->connect->getDb();
+        if(!is_null($pdo)) {
+            $sth = $pdo->prepare($sql);
+            $count = $sth->execute(array(':username' => $comment->username, ':email' => $comment->email, ':body' => $comment->body,  ':id' => $comment->id));
             $pdo = null;
         }
         return $count;
